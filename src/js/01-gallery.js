341ef792290552c876c7b2383244d7e7412f805b
OlenaMurza <murzaelena@gmail.com>
2022-12-25 00:14:03 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
 
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const divGallery = document.querySelector('.gallery');
+
+
 console.log(SimpleLightbox);
 // console.log(galleryItems);
+
+
+function createGalleryLayout(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}" onclick = "return false">
+  <img class="gallery__image" src="${preview}"
+      alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+divGallery.insertAdjacentHTML('afterbegin', createGalleryLayout(galleryItems));
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
